7b90f1ff68f9d02d8993adde4b890169a0e5e588
Ian Yong <ianyong@estl.sg>
2024-05-28 15:29:20 +0800

    Update session ID again before logging after request is processed

--- lib/superlogger/superlogger_middleware.rb
+++ lib/superlogger/superlogger_middleware.rb
@@ -15,7 +15,8 @@ module Superlogger
     end
 
     def process_request(request)
-      setup_logging(request)
+      setup_request_id_for_logging(request)
+      setup_session_id_for_logging(request)
 
       # Start of request
       Rails.logger.info method: request.method, path: request.fullpath
@@ -27,20 +28,24 @@ module Superlogger
 
       # End of request
       duration = ((t2 - t1) * 1000).to_f.round(2)
+
+      # After the request has been processed, the session ID can change from what it was before the request
+      # was processed. As such, we need to setup the session ID again.
+      setup_session_id_for_logging(request)
+
       Rails.logger.info method: request.method, path: request.fullpath, response_time: duration, status: status
 
       [status, _headers, _response]
     end
 
-    def setup_logging(request)
-      if request.env['rack.session']
-        # Store session id before any actual logging is done
-        if request.env['rack.session'].id
-          Superlogger.session_id = request.env['rack.session'].id.to_s
-        end
-      end
-
+    def setup_request_id_for_logging(request)
       Superlogger.request_id = request.uuid.try(:gsub, '-', '')
+    end
+
+    def setup_session_id_for_logging(request)
+      return unless request.env['rack.session']&.id
+
+      Superlogger.session_id = request.env['rack.session'].id.to_s
     end
   end
 end
